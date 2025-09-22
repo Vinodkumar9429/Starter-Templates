88b038260a1ae07fa22248438c96a3018c3bcb6a
Vinodkumar9429 <umr09347@gmail.com>
2025-09-22 09:20:02 +0530

    first commit

---
+++ React + Tailwind V4 latest starter/starter/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div className='font-clash-display font-bold'>
+      <h1>This is ClashDisplay</h1>
+    </div>
+  )
+}
+
+export default App
